185993e5d119d8857eab95abb1887462b72160c1
2016-3-16-che <Artkiller1998@yandex.ru>
2020-05-16 13:37:43 +0300

    + выделение цветом + списки из excel (csv) + смешанные группы + пути

--- src/main/java/ru/web_marks/config/MongoConfig.java
+++ src/main/java/ru/web_marks/config/MongoConfig.java
@@ -1,33 +1,43 @@
 package ru.web_marks.config;
 
 import com.mongodb.MongoClient;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Configuration
+@PropertySource(name = "ConfigurationFromFile", value = "application.properties")
 public class MongoConfig extends AbstractMongoConfiguration {
+
+    @Value("${mongodb.connection.addr:127.0.0.1}")
+    private String u_addr;
+
+    @Value("${mongodb.connection.port:27017}")
+    private Integer u_port;
+
+    @Value("${mongodb.connection.dbname:test}")
+    private String u_dbname;
+
+//    @Value("${mongodb.connection.collection}")
+//    private static String u_collection;
 
     //Задать имя базы данных
     @Override
     protected String getDatabaseName() {
-        return "test";
+        return u_dbname;
     }
 
     //Задать адреса базы данных: IP  и порт
     @Override
     @Bean
     public MongoClient mongoClient() {
-        return new MongoClient("127.0.0.1", 27017);
+        return new MongoClient(u_addr,u_port);
     }
+
 
 
 }
--- src/main/java/ru/web_marks/model/MongoModels.java
+++ src/main/java/ru/web_marks/model/MongoModels.java
@@ -4,13 +4,21 @@
 package ru.web_marks.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.stereotype.Component;
 
 // указан каталог для сохранения объекта
+
+
+
 @Document(collection = "ttt")
+
 public class MongoModels {
+
+
     // генерация уникального ID ( первичный ключ )
     @Id
     protected String id;
