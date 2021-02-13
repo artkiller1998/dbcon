31db09ae79e3cf72f05528dd9f94525e55c35617
2016-3-16-che <Artkiller1998@yandex.ru>
2021-02-13 16:10:03 +0300

    mouse wheels and so on

--- src/main/resources/static/js/create_config.js
+++ src/main/resources/static/js/create_config.js
@@ -15,3 +15,37 @@ $(".remove").click(function() {
         $(this).parent().remove();
     }
 });
+
+$(window).on("wheel", function(e) {
+    focusedEl = document.activeElement;
+    if(focusedEl === excludedEl){
+        //Exclude one specific element for UI comparison
+        return;
+    }
+    if (focusedEl.nodeName='input' && focusedEl.type && focusedEl.type.match(/number/i)){
+        e.preventDefault();
+        var max=null;
+        var min=null;
+        if(focusedEl.hasAttribute('max')){
+            max = focusedEl.getAttribute('max');
+        }
+        if(focusedEl.hasAttribute('min')){
+            min = focusedEl.getAttribute('min');
+        }
+        var value = parseInt(focusedEl.value, 10);
+        if (e.originalEvent.deltaY < 0) {
+            value++;
+            if (max !== null && value > max) {
+                value = max;
+            }
+        } else {
+            value--;
+            if (min !== null && value < min) {
+                value = min;
+            }
+        }
+        focusedEl.value = value;
+    }
+});
+
+
