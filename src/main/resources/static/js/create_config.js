f130fc96651ee1424ae5a3b9afc880108001ea2b
2016-3-16-che <Artkiller1998@yandex.ru>
2021-02-15 00:01:39 +0300

    idealno !!! insert mark into the end

--- src/main/resources/static/js/create_config.js
+++ src/main/resources/static/js/create_config.js
@@ -3,7 +3,41 @@ $(".add").click(function() {
                             find("input").val("").end()
                             .insertBefore("form > p:last-child");
     $('.task-remove-btn').attr('disabled',false);
+
     return false;
+});
+
+$(".add_mark").click(function() {
+    if ($(this).parent().next(".mark_t").length == 0)
+        $("#mark").clone(true).
+        find("input").val("").end()
+            .insertAfter($(this).parent());
+    else
+        $("#mark").clone(true).
+        find("input").val("").end()
+            .insertAfter($(this).parent().next(".mark_t")); //$(this).parent()
+    //$('.task-remove-btn').attr('disabled',false);
+    return false;
+});
+
+$(".remove_mark").click(function() {
+    if ($('#form-task > #mark').length == 1) {
+        $('.mark-remove-btn').attr('disabled',true);
+    }
+    else {
+        $(this).parent().remove();
+        $('.mark-remove-btn').attr('disabled',false);
+    }
+});
+
+$(".remove").click(function() {
+    if ($('#form-task > #task').length == 1) {
+        $('.task-remove-btn').attr('disabled',true);
+    }
+    else {
+        $('.task-remove-btn').attr('disabled',false);
+        $(this).parent().remove();
+    }
 });
 
 function handleSubmit(event) {
@@ -75,108 +109,8 @@ function handleSubmit(event) {
     document.getElementById('download_button').appendChild(a);
 }
 
-
-
 const form = document.querySelector('form');
 form.addEventListener('submit', handleSubmit);
 
 
-// var shipments = [],
-//     task  = {},
-//     _id = {},
-//     mark = {};
-// $(".task").each(function() {
-//     var task = {},
-//         boxes = {};
-//     $(this).children().each(function(){
-//         var boxdata = $(this).data();
-//         mark = {};
-//         $(this).children().each(function(){
-//             var candydata = $(this).data();
-//             mark[candydata["candyid"]] = {
-//                 scale: candydata["color"],
-//                 descr: candydata["flavor"]
-//             };
-//             boxes[boxdata["boxid"]] = candy;
-//         });
-//         //console.log(JSON.stringify(boxes)); // works
-//     });
-//     shipment = {shipment: boxes};
-//     shipments.push(shipment); // for multiples
-// });
-//
-// console.log(JSON.stringify(shipments[0]));
-// console.log(shipments.length);
-
-
-
-$(".add_mark").click(function() {
-    $("#mark").clone(true).
-    find("input").val("").end()
-        .insertAfter($(this).parent());
-    //$('.task-remove-btn').attr('disabled',false);
-    return false;
-});
-
-// $('#add_mark').on('click', function() {
-//     // while ($(this).val() != $('#form-task > #mark').length) {
-//     //     if ($(this).val() > $('#form-task > #mark').length) {
-//     //         add_mark();
-//     //     }
-//     //     else {
-//     //         remove_mark();
-//     //     }
-//     // }
-//
-//      add_mark();
-//     $(this).val() // get the current value of the input field.
-// });
-
-
-
-$(".remove_mark").click(function() {
-    $(this).parent().remove();
-});
-
-$(".remove").click(function() {
-    if ($('#form-task > #task').length == 1) {
-        $('.task-remove-btn').attr('disabled',true);
-    }
-    else {
-        $('.task-remove-btn').attr('disabled',false);
-        $(this).parent().remove();
-    }
-});
-/*
-$(window).on("wheel", function(e) {
-    focusedEl = document.activeElement;
-    if(focusedEl === excludedEl){
-        //Exclude one specific element for UI comparison
-        return;
-    }
-    if (focusedEl.nodeName='input' && focusedEl.type && focusedEl.type.match(/number/i)){
-        e.preventDefault();
-        var max=null;
-        var min=null;
-        if(focusedEl.hasAttribute('max')){
-            max = focusedEl.getAttribute('max');
-        }
-        if(focusedEl.hasAttribute('min')){
-            min = focusedEl.getAttribute('min');
-        }
-        var value = parseInt(focusedEl.value, 10);
-        if (e.originalEvent.deltaY < 0) {
-            value++;
-            if (max !== null && value > max) {
-                value = max;
-            }
-        } else {
-            value--;
-            if (min !== null && value < min) {
-                value = min;
-            }
-        }
-        focusedEl.value = value;
-    }
-});*/
 
