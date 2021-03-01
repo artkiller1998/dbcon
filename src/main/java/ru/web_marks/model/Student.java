4d26de51c079b573b4c6a008014a4dbfbe890e71
2016-3-16-che <Artkiller1998@yandex.ru>
2021-03-01 12:23:11 +0300

    works in docker some troubles with /csv folder

--- src/main/java/ru/web_marks/model/Student.java
+++ src/main/java/ru/web_marks/model/Student.java
@@ -47,11 +47,11 @@ public class Student extends MongoModels{
             InputStreamReader input_csv;
             try {
                 input_csv = new InputStreamReader(new
-                        FileInputStream("src/main/resources/static/csv/" + g_ident + ".csv"), StandardCharsets.UTF_8);
+                        FileInputStream("src/main/resources/static/csv/" + g_ident + ".CSV"), StandardCharsets.UTF_8);
             }
             catch (FileNotFoundException e) {
                 input_csv = new InputStreamReader(new
-                        FileInputStream("../webapps/ROOT/WEB-INF/classes/static/csv/" + g_ident + ".csv"), StandardCharsets.UTF_8);
+                        FileInputStream("../webapps/ROOT/WEB-INF/classes/static/csv/" + g_ident + ".CSV"), StandardCharsets.UTF_8);
             }
 
             br = new BufferedReader(input_csv);
